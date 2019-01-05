5ad967903d3c976922ca6195664891ffdca457d7
chenhao <1005573110@qq.com>
2019-01-05 20:27:47 +0800

    第一次瞎添的

---
+++ src/main/java/com/qianfeng/autodata/Application.java
@@ -0,0 +1,14 @@
+package com.qianfeng.autodata;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
